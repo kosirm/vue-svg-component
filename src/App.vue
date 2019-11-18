8d99f41d5c3592cbdac054a6431cb27bb0516a49
Milan Košir <54502+kosirm@users.noreply.github.com>
2019-11-18 16:53:31 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Logo color="green--outline" :width="150" :strokeWidth="10"/>
+    <Logo color="red" :width="150" :strokeWidth="20"/>
+  </div>
+</template>
+
+<script>
+import Logo from './components/Logo'
+
+export default {
+  name: 'App',
+  components: {
+    Logo
+  }
+}
+</script>
---
+++ src/components/Logo.vue
@@ -0,0 +1,61 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    baseProfile="tiny"
+    version="1.2"
+    :width="width"
+    :height="width"
+    viewBox="0 0 422.1 329.5"
+  >
+    <path
+      :class="color"
+      :stroke-width="strokeWidth"
+      stroke-miterlimit="10"
+      d="M108.2 262.6L220 39c-44.1 0-84.5 24.9-104.2 64.4L4 327c44.2 0 84.5-24.9 104.2-64.4zM306.2 226.1L418 2.5c-44.1 0-84.5 24.9-104.2 64.4L202 290.5c44.2 0 84.5-24.9 104.2-64.4z"
+    ></path>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'Logo',
+  props: {
+    color: {
+      type: String,
+      default: 'white', // green, green--outline, white
+      required: false
+    },
+    strokeWidth: {
+      type: Number,
+      default: 5,
+      required: false
+    },
+    width: {
+      type: Number,
+      default: 50,
+      required: false
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.green {
+  fill: #61ffab;
+  stroke: none;
+}
+.red {
+  fill: #bb3a35;
+  stroke: #091c70;
+}
+
+.green--outline {
+  fill: #354258;
+  stroke: #61ffab;
+}
+
+.white {
+  fill: mintcream;
+  stroke: none;
+}
+</style>
